a34738e2eccc17a311c35cedf884b439a8581db7
joker.huang <ybo.sama@gmail.com>
2017-09-27 14:46:03 +0800

    http-request-info

--- src/http-request-info/main.go
+++ src/http-request-info/main.go
@@ -3,24 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//	"regexp"
+	"net/url"
 	"time"
 
 	"http-request-info/user_agent"
 )
 
-//type MachineInfo struct {
-//	Browser  string
-//	System   string
-//	Kernel   string
-//	Language string
-//}
+func tongji(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("request path:\t", r.URL.Path)
+	fmt.Println("request time:\t", time.Now().Local())
+	fmt.Println("client ip:\t", r.RemoteAddr)
+	fmt.Println("param sessionid:\t", r.FormValue("sessionid"))
+	fmt.Println("param userid:\t", r.FormValue("userid"))
+	fmt.Println("param url:\t", r.FormValue("url"))
+	fmt.Println("param referer:\t", r.FormValue("referer"))
 
-//type ParseMachineInfo(userAgent string) MachineInfo {
-//	m, _ := regexp.MatchString("", userAgent)
+	fmt.Println("------------")
 
-//	fmt.Println(m)
-//}
+	u, err := url.Parse(r.FormValue("referer"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("param referer Host:\t", u.Hostname())
+
+	fmt.Println("------------")
+
+	u, err = url.Parse(r.FormValue("url"))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("param url Path:\t", u.Path)
+
+	fmt.Println("------------")
+
+	ua := user_agent.New(r.UserAgent())
+
+	fmt.Printf("operating platform:\t%v\n", ua.Platform())
+	fmt.Printf("operating system:\t%v\n", ua.OS())
+
+	name, version := ua.Browser()
+	fmt.Printf("browser name:\t%v\n", name)
+	fmt.Printf("browser version:\t%v\n", version)
+
+	fmt.Println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+}
 
 func showInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("request time:\t", time.Now().Local())
@@ -49,6 +77,7 @@ func showInfo(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", showInfo)
+	http.HandleFunc("/visit", tongji)
 
 	err := http.ListenAndServe(":8080", nil)
 
