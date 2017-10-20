86cea96727951fccc60a2032ac55485a228f5e46
joker.huang <ybo.sama@gmail.com>
2017-10-20 17:34:26 +0800

    consumer

--- src/sarama-consumer/main.go
+++ src/sarama-consumer/main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"runtime"
 	"strings"
-	//	"time"
+	"time"
 
 	"github.com/Shopify/sarama"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var _startTime, _endTime string
+
+	flag.StringVar(&_startTime, "start-time", "", "put your start time here(2006-01-02 15:04:05)")
+	flag.StringVar(&_endTime, "end-time", "", "put your end time here(2006-01-02 15:04:05)")
+
+	flag.Parse()
+
+	if flag.NFlag() != 2 {
+		fmt.Println("请使用参数 -start-time 和 -end-time")
+		return
+	}
+
+	startTime, err := time.Parse("2006-01-02 15:04:05", _startTime)
+	if err != nil {
+		fmt.Println("转换开始时间时出错, 错误日志为: ", err.Error())
+		return
+	}
+
+	endTime, err := time.Parse("2006-01-02 15:04:05", _endTime)
+	if err != nil {
+		fmt.Println("转换结束时间时出错, 错误日志为: ", err.Error())
+		return
+	}
+
+	startTimestamp := startTime.Unix()
+	endTimestamp := endTime.Unix()
+
+	if startTimestamp > endTimestamp {
+		fmt.Println("开始时间不能大于结束时间")
+		return
+	}
+
+	fmt.Printf("正在统计[start-time, end-time)中的数据...\n")
 
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true
@@ -35,15 +71,47 @@ func main() {
 	defer consumer.Close()
 	defer client.Close()
 
+	var countPageVisit uint32
+
+	//Out:
 	for {
 		select {
 		case err := <-client.Errors():
 			log.Println("haha: ", err.Error())
 		case msg := <-client.Messages():
-			log.Printf("Partition:%d, Offset:%d\n", msg.Partition, msg.Offset)
+			data := parseRecord(string(msg.Value))
+
+			log.Println(data["server_time"])
+
+			serverTime, err := time.Parse("2006-01-02 15:04:05.6 7", data["server_time"])
+
+			if err != nil {
+				continue
+			}
+
+			log.Println(serverTime, "\n")
+
+			//			if serverTime.Unix() >= startTimestamp && serverTime.Unix() < endTimestamp {
+			//				countPageVisit++
+			//			} else if serverTime.Unix() <= endTimestamp {
+			//				break Out
+			//			}
+
+			//			log.Printf("Partition:%d, Offset:%d\n", msg.Partition, msg.Offset)
 		}
-		//		<-time.After(1 * time.Millisecond)
 	}
 
-	log.Println("+++++++++++++++++++")
+	log.Println("page visit: ", countPageVisit, "\n+++++++++++++++++++")
+}
+
+func parseRecord(record string) map[string]string {
+	data := strings.Split(strings.Trim(record, "\r\n"), "\r\n")
+
+	dataMap := make(map[string]string)
+	for _, v := range data {
+		d := strings.SplitN(v, ":", 2)
+		dataMap[d[0]] = d[1]
+	}
+
+	return dataMap
 }
