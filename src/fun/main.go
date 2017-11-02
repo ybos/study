cf87f0d1ef3648c30e79d018054e5af3dce3a682
joker.huang <ybo.sama@gmail.com>
2017-11-02 17:25:21 +0800

    fun

--- src/fun/main.go
+++ src/fun/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 
 	//	"github.com/garyburd/redigo/redis"
 )
+
+const TopDomain string = "localhost:8080"
+const TopDomainLen int = 14
 
 func route(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
@@ -15,7 +20,35 @@ func route(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMovedPermanently)
 	}
 
-	fmt.Fprint(w, r.URL.Path)
+	domain := []rune(r.Host)
+	domainLen := len(domain)
+
+	domain = domain[:domainLen-TopDomainLen-1]
+
+	res, err := http.Get("http://" + string(domain) + r.URL.RequestURI())
+
+	if err != nil {
+		fmt.Fprintf(w, "请求错误(http://"+string(domain)+r.URL.RequestURI()+")"+err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "解析错误(http://"+string(domain)+r.URL.RequestURI()+")"+err.Error())
+		return
+	}
+
+	str := strings.Replace(string(body), string(domain), r.Host, -1)
+
+	fmt.Fprintf(w, str)
+
+	// 处理请求头
+	//	w.Header().Set("Content-Type", res.Header["Content-Type"])
+	w.Header().Set("Content-Type", res.Header["Content-Type"][0])
+	fmt.Println(res.Header["Content-Type"][0])
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -30,12 +63,14 @@ func safe(w http.ResponseWriter, r *http.Request) {
 	}
 
 	urlData, err := url.Parse(r.FormValue("link"))
+
 	if err != nil {
 		t, _ := template.ParseFiles("template/index.html")
 		t.Execute(w, "无法解析您所提交的连接")
 	}
 
-	fmt.Fprintf(w, "Hostname: %s<br />Path: %s<br />Query: %s", urlData.Hostname(), urlData.Path, urlData.Query())
+	w.Header().Set("Location", "http://"+urlData.Hostname()+"."+TopDomain+urlData.RequestURI())
+	w.WriteHeader(http.StatusMovedPermanently)
 }
 
 func main() {
