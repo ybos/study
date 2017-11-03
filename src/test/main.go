d793d5c81b300e716743b59c0c1f0659496ba897
joker.huang <ybo.sama@gmail.com>
2017-11-03 18:26:35 +0800

    postform

--- src/test/main.go
+++ src/test/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func mixer(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println(r.PostForm)
+}
 
 func main() {
-	fmt.Println(3 / 2)
+	http.HandleFunc("/", mixer)
+
+	err := http.ListenAndServe(":80", nil)
+
+	if err != nil {
+		fmt.Println("Fatal error: ", err.Error())
+	}
+
+	res, err := http.PostForm("http://account.ecovacs.cn/account/member/signin", url.Values{"account": {"test"}, "password": {"test"}})
+
+	if err != nil {
+		fmt.Println("请求错误" + err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("解析错误" + err.Error())
+		return
+	}
+
+	fmt.Println(string(body))
 }
