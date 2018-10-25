fd0ed0e0a16aa6de8b1cf99cfb3d7c090a40915b
joker.huang <ybo.sama@gmail.com>
2018-10-25 13:28:25 +0800

    wrap a type

--- src/imooc_wrap_type/main.go
+++ src/imooc_wrap_type/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 type Queue []int
+type Queue2 []int
+
+// Cannot change the original data
+func (q Queue2) Push(v int) {
+	q = append(q, v)
+}
 
 // 压入值
 func (q *Queue) Push(v int) {
@@ -37,4 +43,13 @@ func main() {
 	fmt.Println(q.Pop())
 
 	fmt.Println(q.IsEmpty())
+
+	fmt.Println("--------------")
+
+	q2 := Queue2{1}
+
+	q2.Push(2)
+	q2.Push(3)
+
+	fmt.Println(q2)
 }
